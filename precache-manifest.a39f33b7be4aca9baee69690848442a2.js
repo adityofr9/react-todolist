1c1a9ba4e3946a58f4545115bf116f0202575846
Muhammad Adityo Fathur Rahi <chelseatiyo9@gmail.com>
2022-06-24 14:47:40 +0700

    Updates

---
+++ precache-manifest.a39f33b7be4aca9baee69690848442a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c756691324f307b832524fa9cf9ab0f",
+    "url": "/react-todolist/index.html"
+  },
+  {
+    "revision": "506e7cb1ada9ffdbca0d",
+    "url": "/react-todolist/static/css/main.e4833d58.chunk.css"
+  },
+  {
+    "revision": "8e49ff261b0b40148c6b",
+    "url": "/react-todolist/static/js/2.c6b53d6d.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-todolist/static/js/2.c6b53d6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "506e7cb1ada9ffdbca0d",
+    "url": "/react-todolist/static/js/main.3547a6bc.chunk.js"
+  },
+  {
+    "revision": "3ca8da903cb76f262589",
+    "url": "/react-todolist/static/js/runtime-main.324cd047.js"
+  },
+  {
+    "revision": "dc8e4e723768e9595120f5f375ae28a2",
+    "url": "/react-todolist/static/media/traveling-doodle.dc8e4e72.webp"
+  }
+]);
